9ef203f960d96804e0379f7c62627e106ebcedc6
Maher Magdy <mahermagdy543@gmail.com>
2024-01-19 06:25:36 +0200

    ChatApplication

---
+++ ClientApp/src/app/join-room/join-room.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ChatService } from '../chat.service';
+
+@Component({
+  selector: 'app-join-room',
+  //standalone: true,
+  //imports: [ReactiveFormsModule],
+  templateUrl: './join-room.component.html',
+  styleUrls: ['./join-room.component.css'],
+  //providers:[ChatService]
+
+})
+export class JoinRoomComponent implements OnInit {
+
+  joinRoomForm!: FormGroup;
+  fb = inject(FormBuilder);
+  router = inject(Router);
+  chatService = inject(ChatService);
+
+  ngOnInit(): void {
+    this.joinRoomForm = this.fb.group({
+      user: ['', Validators.required],
+      room: ['', Validators.required]
+    });
+  }
+
+  joinRoom(){
+    const {user, room} = this.joinRoomForm.value;
+    sessionStorage.setItem("user", user);
+    sessionStorage.setItem("room", room);
+    this.chatService.joinRoom(user, room)
+    .then(()=>{
+      this.router.navigate(['chat']);
+    }).catch((err)=>{
+      console.log(err);
+    })
+
+  }
+
+}
